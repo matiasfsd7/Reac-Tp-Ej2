b2663290d2c32bb9d0f1491c127dc653d4d1b69c
matiasfsd7 <matias.n.ortega@gmail.com>
2023-07-19 23:42:20 -0300

    termine el ej2

---
+++ src/Friend.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Friend = ({ amigo }) => {
+  return (
+    <div>
+      <h1 className="text-center text-light"> Hello {amigo}</h1>
+    </div>
+  );
+};
+
+export default Friend;
